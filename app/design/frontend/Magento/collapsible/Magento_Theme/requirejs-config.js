9b68f49f68f8998f38cc6343fb7b2be70d8b5e88
Markizdickabraz <mark.sahaxkiy@gmail.com>
2025-01-20 10:18:08 +0200

    add theme collapsible, collapsible widget, collapsible mixin ajax.

---
+++ app/design/frontend/Magento/collapsible/Magento_Theme/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'mage/collapsible': {
+                'Magento_Theme/js/collapsible-mixin': true
+            }
+        }
+    }
+};
---
+++ app/design/frontend/Magento/collapsible/Magento_Theme/web/js/collapsible-mixin.js
@@ -0,0 +1,54 @@
+define([
+    'jquery',
+    'mage/storage'
+], function ($, storage) {
+    'use strict';
+
+    return function (originalWidget) {
+        $.widget('mage.collapsible', originalWidget, {
+            _create: function () {
+                this._super();
+                this._bindSearchTop();
+            },
+
+            _bindSearchTop: function () {
+                const titleElement = this.element.find('[data-role="title"]');
+                const resultList = this.element.find('ul');
+
+                titleElement.on('click', () => {
+                    const colTitle = $('.collapsible__title').attr('aria-selected');
+                    if (colTitle === 'false') {
+                        return;
+                    } else {
+                        this._fetchTopResults(resultList);
+                    }
+                });
+            },
+
+            _fetchTopResults: function (resultList) {
+                resultList.empty().append('<li>Loading...</li>');
+
+                const query = 'top';
+                const url = `search/ajax/suggest/?q=${query}`;
+
+                storage.get(url)
+                    .done((response) => {
+                        resultList.empty();
+
+                        if (response && response.results && response.results.length) {
+                            response.results.forEach(item => {
+                                resultList.append('<li>' + item + '</li>');
+                            });
+                        } else {
+                            resultList.append('<li>No results found</li>');
+                        }
+                    })
+                    .fail(() => {
+                        resultList.empty().append('<li>Error fetching results</li>');
+                    });
+            }
+        });
+
+        return $.mage.collapsible;
+    };
+});
